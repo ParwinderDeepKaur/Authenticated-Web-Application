da653a7173ce6e2cc4cf0783701182db359407dc
ParwinderDeepKaur <A00237487@mycambrian.ca>
2022-03-29 09:05:05 -0400

    created new project with registration and login

---
+++ securityAssign/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using securityAssign.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace securityAssign
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    using (var client = new ApplicationDbContext())
+                    {
+                        client.Database.EnsureCreated();
+                    }
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
